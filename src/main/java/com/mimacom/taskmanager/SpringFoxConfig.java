39adf12d1c4389342e23535b11b22d87f9c1178d
Carlos Cortina <carlos.cortina@mimacom.com>
2021-04-22 12:21:11 +0200

    Adding Swagger configuration

---
+++ src/main/java/com/mimacom/taskmanager/SpringFoxConfig.java
@@ -0,0 +1,42 @@
+package com.mimacom.taskmanager;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                //.apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.mimacom.taskmanager.controllers"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(getApiInfo())
+                ;
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfo(
+                "Task Manager API",
+                "This is a Task Manager developed for mimacom",
+                "1.0",
+                "termsOfServiceUrl",
+                new Contact("Carlos", "https://localhost", "carlos.cortina@mimacom.com"),
+                "License",
+                "License url",
+                Collections.emptyList()
+        );
+    }
+}
